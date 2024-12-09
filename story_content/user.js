e270be6c2bf8da6a1369f3c46fc37feffb725891
Rakhmayanti <rakhmayanti.2024@student.uny.ac.id>
2024-12-09 09:55:05 +0700

    Add files via upload

---
+++ story_content/user.js
@@ -0,0 +1,99 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5zcSImw96aH":
+        Script1();
+        break;
+      case "6JtMEcGIbQR":
+        Script2();
+        break;
+      case "5rOMR7Xhz9p":
+        Script3();
+        break;
+      case "6et9cAECDjZ":
+        Script4();
+        break;
+      case "6d4fgFnqhbF":
+        Script5();
+        break;
+      case "60IYCT9jZVj":
+        Script6();
+        break;
+      case "62tLXfCZUvn":
+        Script7();
+        break;
+      case "5j8ldFVegCz":
+        Script8();
+        break;
+  }
+}
+
+function Script1()
+{
+  var audio = document.getElementById('bgSong');
+
+audio.src="musik.mp3";
+
+audio.load();
+
+audio.play();
+
+audio.volume=0.3;
+}
+
+function Script2()
+{
+  var audio = document.getElementById('bgSong');
+
+audio.src="musik.mp3";
+
+audio.load();
+
+audio.play();
+
+audio.volume=0.3;
+}
+
+function Script3()
+{
+  var audio = document.getElementById('bgSong');
+
+audio.volume = 0.3;
+}
+
+function Script4()
+{
+  var audio = document.getElementById('bgSong');
+
+audio.volume = 0.3;
+}
+
+function Script5()
+{
+  var audio = document.getElementById('bgSong')
+;
+audio.volume = 0.5;
+}
+
+function Script6()
+{
+  var audio = document.getElementById('bgSong');
+
+audio.volume = 0.7;
+}
+
+function Script7()
+{
+  var audio = document.getElementById('bgSong');
+
+audio.volume = 0.9;
+}
+
+function Script8()
+{
+  var audio = document.getElementById('bgSong');
+
+audio.volume = 0.3;
+}
+
